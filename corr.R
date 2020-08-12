706c1b4e530ef0807594e38da61675ff13051f77
SuperDope <manan@indianterrapin.com>
2020-08-11 22:45:17 -0400

    working function

--- corr.R
+++ corr.R
@@ -9,5 +9,26 @@ corr <- function(directory, threshold = 0){
   
   ## Return a numeric vector of correlations
   ## NOTE: Do not round the result!
-
+  cor_results <- numeric(0)
+  
+  complete_cases <- complete(directory)
+  complete_cases <- complete_cases[complete_cases$nobs>=threshold, ]
+  #print(complete_cases["id"])
+  #print(unlist(complete_cases["id"]))
+  #print(complete_cases$id)
+  
+  if(nrow(complete_cases)>0){
+    for(i in complete_cases$id){
+      path <- paste(getwd(), "/", directory, "/", sprintf("%03d", i), ".csv", sep = "")
+      #print(path)
+      i_data <- read.csv(path)
+      #print(i_data)
+      interested_data <- i_data[(!is.na(i_data$sulfate)), ]
+      interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+      sulfate_data <- interested_data["sulfate"]
+      nitrate_data <- interested_data["nitrate"]
+      cor_results <- c(cor_results, cor(sulfate_data, nitrate_data))
+    }
+  }
+  cor_results
 }
