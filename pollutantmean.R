ce710b0648ed3f685076d2e7a06354ca6f568ce4
SuperDope <manan@indianterrapin.com>
2020-08-11 22:08:23 -0400

    Working version

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,5 +1,26 @@
 # Pollutant mean function for Week 2 assignment
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
   
+  ## 'pollutant is a character vector of length 1 indicating
+  ## the name of the pollutant for which we will calculate the
+  ## mean; either "sulfate" or "nitrate".
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## return the mean of the pollutant across all monitors list
+  ## in the 'id' vector (ignoring NA values)
+  ## NOTE: Do not round the results!
+  means <- c()
+  
+  for(monitor in id){
+    path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+    monitor_data <- read.csv(path)
+    interested_data <- monitor_data[pollutant]
+    means <- c(means, interested_data[!is.na(interested_data)])
+  }
+  
+  mean(means)
 }
